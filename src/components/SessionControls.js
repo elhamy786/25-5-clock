13cdbd10d28b98b127f8f74612fefff55ddd54ce
elhamy786 <elham.afzali1383@gmail.com>
2024-09-13 15:51:42 +0430

    Edit SessionControls.js file

--- src/components/SessionControls.js
+++ src/components/SessionControls.js
@@ -4,14 +4,16 @@ import { incrementSession, decrementSession } from '../redux/actions';
 
 const SessionControls = () => {
   const dispatch = useDispatch();
-  const sessionLength = useSelector(state => state.sessionLength);
+  const sessionLength = useSelector(state => state.sessionLength); // Added parentheses around 'state'
 
   return (
     <div>
       <h2 id="session-label">Session Length</h2>
       <button id="session-decrement" onClick={() => dispatch(decrementSession())}>-</button>
+       {/* Added type="button" */}
       <span id="session-length">{sessionLength}</span>
       <button id="session-increment" onClick={() => dispatch(incrementSession())}>+</button>
+       {/* Added type="button" */}
     </div>
   );
 };
