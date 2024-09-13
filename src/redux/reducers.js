b816c9c4932f598e29083bb91d5d8195b00b432f
elhamy786 <elham.afzali1383@gmail.com>
2024-09-13 16:56:15 +0430

    solve linters error

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,61 +1,61 @@
 const initialState = {
-    breakLength: 5,
-    sessionLength: 25,
-    timeLeft: 1500,
-    timerLabel: 'Session',
-    isRunning: false,
-    isSession: true,
-  };
-  
-  function clockReducer(state = initialState, action) {
-    switch (action.type) {
-      case 'INCREMENT_BREAK':
+  breakLength: 5,
+  sessionLength: 25,
+  timeLeft: 1500,
+  timerLabel: 'Session',
+  isRunning: false,
+  isSession: true,
+};
+
+function clockReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'INCREMENT_BREAK':
+      return {
+        ...state,
+        breakLength: Math.min(state.breakLength + 1, 60),
+      };
+
+    case 'DECREMENT_BREAK':
+      return {
+        ...state,
+        breakLength: Math.max(state.breakLength - 1, 1),
+      };
+
+    case 'INCREMENT_SESSION':
+      return {
+        ...state,
+        sessionLength: Math.min(state.sessionLength + 1, 60),
+        imeLeft: !state.isRunning ? (state.sessionLength + 1) * 60 : state.timeLeft,
+      };
+
+    case 'DECREMENT_SESSION':
+      return {
+        ...state,
+        sessionLength: Math.max(state.sessionLength - 1, 1),
+        timeLeft: !state.isRunning ? (state.sessionLength - 1) * 60 : state.timeLeft,
+      };
+
+    case 'START_STOP':
+      return {
+        ...state,
+        isRunning: !state.isRunning,
+      };
+
+    case 'RESET':
+      return {
+        ...initialState,
+        timeLeft: initialState.sessionLength * 60,
+      };
+
+    case 'TICK':
+      if (state.timeLeft === 0) {
         return {
           ...state,
-          breakLength: Math.min(state.breakLength + 1, 60),
+          isSession: !state.isSession,
+          timerLabel: state.isSession ? 'Break' : 'Session',
+          timeLeft: state.isSession ? state.breakLength * 60 : state.sessionLength * 60,
         };
-  
-      case 'DECREMENT_BREAK':
-        return {
-          ...state,
-          breakLength: Math.max(state.breakLength - 1, 1),
-        };
-  
-      case 'INCREMENT_SESSION':
-        return {
-          ...state,
-          sessionLength: Math.min(state.sessionLength + 1, 60),
-          timeLeft: !state.isRunning ? (state.sessionLength + 1) * 60 : state.timeLeft,
-        };
-  
-      case 'DECREMENT_SESSION':
-        return {
-          ...state,
-          sessionLength: Math.max(state.sessionLength - 1, 1),
-          timeLeft: !state.isRunning ? (state.sessionLength - 1) * 60 : state.timeLeft,
-        };
-  
-      case 'START_STOP':
-        return {
-          ...state,
-          isRunning: !state.isRunning,
-        };
-  
-      case 'RESET':
-        return {
-          ...initialState,
-          timeLeft: initialState.sessionLength * 60,
-        };
-  
-      case 'TICK':
-        if (state.timeLeft === 0) {
-          return {
-            ...state,
-            isSession: !state.isSession,
-            timerLabel: state.isSession ? 'Break' : 'Session',
-            timeLeft: state.isSession ? state.breakLength * 60 : state.sessionLength * 60,
-          };
-        }
+      }
         return {
           ...state,
           timeLeft: state.timeLeft - 1 > 0 ? state.timeLeft - 1 : 0,
