db98e144464312574d154527422dde87f3ff764c
elhamy786 <elham.afzali1383@gmail.com>
2024-09-13 15:45:04 +0430

    Edit BreakControls.js file

--- src/components/BreakControls.js
+++ src/components/BreakControls.js
@@ -4,14 +4,16 @@ import { incrementBreak, decrementBreak } from '../redux/actions';
 
 const BreakControls = () => {
   const dispatch = useDispatch();
-  const breakLength = useSelector((state) => state.breakLength);
+  const breakLength = useSelector((state) => state.breakLength); // Added parentheses around 'state'
 
   return (
     <div>
       <h2 id="break-label">Break Length</h2>
       <button id="break-decrement" type="button" onClick={() => dispatch(decrementBreak())}>-</button>
+      {/* Added type="button" */}
       <span id="break-length">{breakLength}</span>
-      <button id="break-increment" type="button" onClick={() => dispatch(incrementBreak())}>+</button>
+      <button id="break-increment" type="button" onClick={() => dispatch(incrementBreak())}>+</button> 
+      {/* Added type="button" */}
     </div>
   );
 };
